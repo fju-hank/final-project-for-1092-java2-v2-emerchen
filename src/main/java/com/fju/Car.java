878571c8c16dc9493e76691de619b91080bdc066
emerchen <luffy70632@gmail.com>
2021-06-27 01:32:30 +0800

    time calculate function added

---
+++ src/main/java/com/fju/Car.java
@@ -0,0 +1,13 @@
+package com.fju;
+
+import java.util.Date;
+
+public class Car {
+    String id;
+    Date entertime;
+
+    public Car(String id , Date entertime){
+        this.id = id;
+        this.entertime =new Date();
+    }
+}
--- src/main/java/com/fju/Tester.java
+++ src/main/java/com/fju/Tester.java
@@ -2,23 +2,34 @@ package com.fju;
 
 import java.sql.SQLOutput;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Tester {
 
     public static void main(String[] args) {
-        Calendar InTime = Calendar.getInstance();
 
-//進場
+        Calendar Time = Calendar.getInstance();
+        System.out.println("Please enter plate number or enter [pay] to pay");
         Scanner scan = new Scanner(System.in);
-        System.out.println("Please enter plate number:");
-        String PlateNumber = scan.next();
-        Car car1 = new Car(PlateNumber, InTime.getTime());
-        System.out.println("Welcome! " + PlateNumber + " Your Enter Time Is : " + InTime.getTime());
+        String s = scan.next();
+        Car c1 = new Car(s, Time.getTime());
 
-
+        if (s.equals("pay")) {
 //出場
-        System.out.println("Please enter your plate number: ");
+            System.out.println("Please enter your plate number to pay: ");
+            String PlateNumberOut = scan.next();
+            System.out.println(PlateNumberOut + " Total :");
 
+
+//時間計算
+            Date now = new Date();
+            long time = (System.currentTimeMillis() - c1.entertime.getTime()) / 1000 / 60;
+            System.out.println(time);
+        } else {
+//入場
+            System.out.println("Welcome! " + s + "\nYour Enter Time Is : " + Time.getTime());
+
+        }
     }
 }
