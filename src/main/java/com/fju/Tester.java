9dfae7a45da20edd5d15ef2aae08a96b6c8455a7
emerchen <luffy70632@gmail.com>
2021-06-26 23:46:38 +0800

    Class Car and entering function added

--- src/main/java/com/fju/Tester.java
+++ src/main/java/com/fju/Tester.java
@@ -1,7 +1,24 @@
 package com.fju;
 
+import java.sql.SQLOutput;
+import java.util.Calendar;
+import java.util.Scanner;
+
 public class Tester {
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Calendar InTime = Calendar.getInstance();
+
+//進場
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter plate number:");
+        String PlateNumber = scan.next();
+        Car car1 = new Car(PlateNumber, InTime.getTime());
+        System.out.println("Welcome! " + PlateNumber + " Your Enter Time Is : " + InTime.getTime());
+
+
+//出場
+        System.out.println("Please enter your plate number: ");
+
     }
 }
